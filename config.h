e7cfa48ed3f2c5ab30061fa4f01b446cabbe3c63
David Chen <weichen2000121@gmail.com>
2019-10-31 10:02:54 -0700

    anysize patch

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Source Code Pro:pixelsize=23:antialias=true:autohint=true";
+static char *font = "Source Code Pro:pixelsize=24:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
