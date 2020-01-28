3af71f6f39b7ec514d09adff10d898ead13fa6f9
David Chen <weichen2000121@gmail.com>
2020-01-28 01:35:00 -0500

    font name change

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "SauceCodePro NF:pixelsize=24:antialias=true:autohint=true";
+static char *font = "SauceCodePro Nerd Font Mono:pixelsize=24:antialias=true:autohint=true";
 static int borderpx = 4;
 
 /*
