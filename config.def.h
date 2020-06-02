b3217f638b8803df6d18e7b17056bfad9f62eb86
David Chen <weichen2000121@gmail.com>
2020-06-02 12:16:24 -0400

    scrollback patch

--- config.def.h
+++ config.def.h
@@ -212,6 +212,8 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
+	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
 };
 
 /*
